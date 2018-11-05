5f87061d09453c0f37d548637f6d504d99f7bde9
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-11-04 23:13:04 -0600

    working on keyframes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Reset.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <h1>depth</h1>
+        <h3>studio</h3>
       </div>
     );
   }
